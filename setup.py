d96ec15554913a27669c8078ab2a0acd238f6c6c
CalumMallorie <calum.mallorie@ed.ac.uk>
2025-05-07 11:12:52 +0100

    Update Docker workflow for version tags, add project context and bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="weight-tracker",
-    version="0.1.0",
+    version="2.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
